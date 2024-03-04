105be82a989353eea1d2a37c922ff79ff1f1b480
Jolvera2001 <jolvera6@stedwards.edu>
2024-03-04 14:04:44 -0600

    small silly calculator project for practice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut response: String = String::new();
+
+    loop {
+        println!("Add, Subtract, Divide or Multiply? (\"exit\" to stop)");
+        io::stdin()
+            .read_line(&mut response)
+            .expect("Failed to read line");
+        println!("{response}");
+
+        if response.trim() == "Add" {
+            println!("Give 2 numbers to add!");
+            let num1 = take_int();
+            let num2 = take_int();
+            let answ = add(num1, num2);
+            println!("Answer: {answ}")
+        }
+
+        else if response.trim() == "Subtract" {
+            let num1 = take_int();
+            let num2 = take_int();
+            let answ = subtract(num1, num2);
+            println!("Answer: {answ}")
+        }
+
+        else if response.trim() == "Divide" {
+            let num1 = take_int();
+            let num2 = take_int();
+            let answ = divide(num1, num2);
+            println!("Answer: {answ}")
+        }
+
+        else if response.trim() == "Multiply" {
+            let num1 = take_int();
+            let num2 = take_int();
+            let answ = multiply(num1, num2);
+            println!("Answer: {answ}")
+        }
+
+        else if response.trim() == "exit" {
+            break;
+        }
+
+        else {
+            println!("Not a valid input")
+        }
+    }
+}
+
+
+fn take_int() -> usize {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    return input.trim().parse().unwrap();
+}
+
+fn add(a: usize, b: usize) -> usize {
+    a + b
+}
+
+fn subtract(a: usize, b: usize) -> usize {
+    a - b
+}
+
+fn multiply(a: usize, b: usize) -> usize {
+    a * b
+}
+
+fn divide(a: usize, b: usize) -> usize {
+    if b == 0 {
+        println!("Cannot divide by 0!");
+        0
+    }
+
+    else {
+        a / b
+    }
 }
