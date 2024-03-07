b33fd595dc911e3b91abc389c9ae155a304b4745
Jolvera2001 <jolvera6@stedwards.edu>
2024-03-07 12:00:41 -0600

    Playing around with props and state

--- smallFullStack/vueProj/src/App.vue
+++ smallFullStack/vueProj/src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-
+import ButtonCounter from './components/CounterButton.vue'
 </script>
 
 <template>
     <div class="navbar bg-base-100">
-        <div class="flex-none py-2 ms-5">
+        <div class="flex-none ms-5">
             <div class="drawer">
                 <input id="my-drawer" type="checkbox" class="drawer-toggle" />
                 <div class="drawer-content">
@@ -23,8 +23,14 @@
             <h2>Welcome!</h2>
         </div>
     </div>
-    <footer class="footer p-10 bg-neutral text-neutral-content">
-
+    <body>
+        <div class="md:container md:mx-auto ps-10 py-2">
+            <h1>Dashboard</h1>
+            <ButtonCounter word="Scrimblo" />
+        </div>
+    </body>
+    <footer class="footer p-10 bg-neutral text-neutral-content my-5">
+        <a>Placeholder</a>
     </footer>
 </template>
 
