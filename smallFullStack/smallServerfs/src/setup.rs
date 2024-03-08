f448216ea3cdf7bf2789e6a66cc74fddc4af8ebc
Jolvera2001 <jolvera6@stedwards.edu>
2024-03-08 17:00:31 -0600

    Restarted

--- smallFullStack/smallServerfs/src/setup.rs
+++
@@ -1,9 +0,0 @@
-use sea_orm::*;
-
-const DATABASE_URL: &str = "postgresql://jolvera6:L49hbFIXpqlo@ep-lively-glitter-a5akpl5b.us-east-2.aws.neon.tech/AplHelp?sslmode=require";
-const DATABASE_NAME: &str = "AplHelp";
-
-pub(super) async fn set_up_db() -> Result<DatabaseConnection, DbErr> {
-    // connecting to db
-    let db = Database::connect(DATABASE_URL).await?;
-}
